92a751460bd072f7cab113085daf9095ce919a28
JerryWang0122 <school832@gmail.com>
2024-05-19 11:23:02 +0800

    Feat: 購物車DataTables加入RWD，但是品質不是很穩定；加入去申請按鈕事件

--- js/shop-cart.js
+++ js/shop-cart.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   // TODO: 1. 拉LocalStorage內的資料 2. 將商品代碼一致的合併
 
   // 設定DataTables顯示資料
-  const data = [
+  let data = [
     {
       "productId": 1,
       "productImg": "product-1.png",
@@ -20,42 +20,42 @@ $(document).ready(function () {
       "action": "刪除"
     },
     {
-      "productId": 2,
+      "productId": 3,
       "productImg": "product-5.png",
       "productName": "垃圾袋特大",
       "applyQty": 10,
       "action": "刪除"
     },
     {
-      "productId": 2,
+      "productId": 4,
       "productImg": "product-7.png",
       "productName": "垃圾袋特大",
       "applyQty": 10,
       "action": "刪除"
     },
     {
-      "productId": 2,
+      "productId": 5,
       "productImg": "product-9.png",
       "productName": "垃圾袋特大",
       "applyQty": 10,
       "action": "刪除"
     },
     {
-      "productId": 2,
+      "productId": 6,
       "productImg": "product-11.png",
       "productName": "垃圾袋特大",
       "applyQty": 10,
       "action": "刪除"
     },
     {
-      "productId": 2,
+      "productId": 7,
       "productImg": "product-13.png",
       "productName": "垃圾袋特大",
       "applyQty": 10,
       "action": "刪除"
     },
     {
-      "productId": 2,
+      "productId": 8,
       "productImg": "product-15.png",
       "productName": "垃圾袋特大",
       "applyQty": 10,
@@ -64,7 +64,16 @@ $(document).ready(function () {
   ]
 
   // DataTables
-  $('#cartItemsTable').DataTable({
+  let table = $('#cartItemsTable').DataTable({
+    "responsive": {
+      breakpoints: [
+        { name: 'desktop', width: Infinity },
+        { name: 'tablet-l', width: 1024 },//原本是768~1024不含768
+        { name: 'tablet-p', width: 767 },//
+        { name: 'mobile-l', width: 480 },
+        { name: 'mobile-p', width: 320 }
+      ]
+    },
     "searching": false, // 預設為true 搜尋功能，若要開啟不用特別設定
     "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]], //顯示筆數設定
     "stateSave": true, // 預設為false 在頁面刷新時，是否要保存當前表格資料與狀態
@@ -82,36 +91,39 @@ $(document).ready(function () {
       url: "../zh-Hant.json"  // 引用自定義漢化方式
     },
     "columns": [ //列的標題一般是從DOM中讀取（也可以使用這個屬性為表格創建列標題)
-      { data: 'productId', title: "序號" },
       { data: 'productImg',
         title: "圖片",
         render: function (data, type, row) {
           return `<img src="../img/products/${data}" class="cart-product-img m-auto" alt="">`;
-        }
+        }, 
+        className: "min-tablet-l text-center align-middle fs-5"
       },
       { data: 'productName', title: "名稱" },
       { data: 'applyQty', title: "申請量" },
       { data: 'action', 
         title: "動作",
         render: function (data, type, row) {
-          return `<button type="button" class="btn btn-danger">刪除</button>`
-        }
-      }
-    ],
-    columnDefs:[
-      {
-        targets: 0,   // id 列
-        className: 'd-none'
+          return `<button type="button" class="delete-btn btn btn-danger">刪除</button>`
+        },
+        className: "min-tablet-p text-center align-middle fs-5"
       },
+      { data: 'productId', title: "序號" }
+    ],
+    "columnDefs":[
       {
-        targets: 1,   // img 列
-        className: 'fs-5 text-center w-25'
+        targets: -1,   // id 列
+        className: 'd-none'
       },
       {
         targets: '_all',
         className: 'text-center align-middle fs-5'
       }
     ]
+  });
+
+  $('#cartItemsTable tbody').on('click', '.delete-btn', async() => {
+
+    
   });
 
   // 跟DataTable一起完成，生成"去申請"按鈕
@@ -121,6 +133,14 @@ $(document).ready(function () {
   applyBtn.innerText = '去申請';
   $('#cartItemsTable').after(applyBtn);
 
-
+  $('#applyMaterialsBtn').on('click', function () {
+    let processedData = data.map(item => {
+      return {
+        productId: item.productId,
+        applyQty: item.applyQty
+      };
+    });
+    console.log(processedData);
+  });
 
 });
