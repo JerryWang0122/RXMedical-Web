1f2040cfd186d8fa6a959fb33a5e74b87211ec01
JerryWang0122 <school832@gmail.com>
2024-05-22 23:08:40 +0800

    feat: js控制 後台-會員權限 頁面顯示

---
+++ js/back-page.js
@@ -0,0 +1,58 @@
+
+/* 頁面路由 (待 DOM 加載完成之後再執行)
+ *
+ *
+*/
+$(document).ready(async() => {
+  await loadHTML('/f-order_history.html', '#contentArea');
+
+  // 按下navbar的標題時，回到首頁
+  $('#backIndexBtn').on('click', () => location.reload());
+
+  // -------------------- navbar 按鈕被按下時 -----------------
+  $('#navbarMenu').on('click', async(event) => {
+    await handleChangeNavBar(event, '#orderListManageBtn', '/f-user_info.html');
+    await handleChangeNavBar(event, '#materialSalesBtn', '/f-order_history.html');
+    await handleChangeNavBar(event, '#memberAuthLevelBtn', '/b-member_auth.html');
+  })
+
+
+});
+
+
+
+
+
+
+
+
+const BASE_URL = '';
+
+// 在target容器中載入特定HTML
+const loadHTML = async(url, target) => {
+  const fullURL = BASE_URL + url;
+
+  try {
+    const response = await fetch(fullURL);
+    const data = await response.text();
+    $('#contentArea').html(data);
+  } catch (err) {
+    console.error(err);
+  }
+
+};
+
+// 處理navBar點擊，切換頁面效果
+const handleChangeNavBar = async (event, idName, url) => {
+  const eventId = '#' + event.target.id;
+  // if eventId not equals to idName => return
+  if (eventId !== idName) {
+    return;
+  }
+  $('#navbarMenu .navbar-nav .nav-link').removeClass('active');
+  // 将点击的按钮添加 'active' 类
+  $(event.target).addClass('active');
+  await loadHTML(url, '#contentArea');
+};
+
+
